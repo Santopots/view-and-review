090ad640396a3aa8dc2bdb3f6fa89a9fc58e55a3
Christian López García <68792480+drownedd@users.noreply.github.com>
2023-03-20 12:05:48 +0100

    added initial film model

--- project/web/models.py
+++ project/web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Film(models.Model):
+    title = models.CharField(primary_key=True, max_length=64)
+    release_date = models.DateField()
+    duration = models.IntegerField()
+    plot = models.CharField(max_length=512)
+    
+    # director = models.CharField(max_length=64, foreign_ke)
+    # language
+    # genre
+    # actor
+    def __str__(self):
+        return self.title
+    
